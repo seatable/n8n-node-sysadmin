2af02ea8d3311b7ee65c1def82b0a7cfaa4291ae
Christoph Dyllick-Brenzinger <cdb@datamate.org>
2024-05-08 17:03:51 +0200

    fix sysinfo

--- nodes/SeaAdmin/v1/actions/system_info/get_system_info/execute.ts
+++ nodes/SeaAdmin/v1/actions/system_info/get_system_info/execute.ts
@@ -5,17 +5,18 @@ export async function get_system_info(
 	this: IExecuteFunctions,
 	index: number,
 ): Promise<INodeExecutionData[]> {
-	// get URL
 	const credentials = await this.getCredentials('seaadmin');
 	const baseURL = credentials?.domain || 'https://cloud.seatable.io';
 
 	const options: OptionsWithUri = {
 		method: 'GET',
 		qs: {},
-		body: {
-		},
+		body: {},
 		uri: baseURL + '/api/v2.1/admin/sysinfo/',
+		json: true,
 	};
+
 	const responseData = await this.helpers.requestWithAuthentication.call(this, 'seaadmin', options);
+
 	return this.helpers.returnJsonArray(responseData as IDataObject[]);
 }
